11d9631a5a705d5d2ee03c054162545977092801
Alexander Wolf <alexander.wolf@8mylez.com>
2026-04-22 15:43:12 +0200

    chore: initial skeleton (free-disk-space controller + Shopware plugin shell)

---
+++ src/Api/MonitorioApiController.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Emz\Monitorio\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(defaults={"_routeScope"={"api"}})
+ */
+class MonitorioApiController extends AbstractController
+{
+    /**
+     * @Route(
+     *     "/api/monitorio/free-disk-space",
+     *     name="api.monitorio.free_disk_space",
+     *     methods={"GET"}
+     * )
+     *
+     * @return JsonResponse
+     */
+    public function getFreeDiskSpace(): JsonResponse
+    {
+        $df = disk_free_space("/");
+
+        $dt = disk_total_space("/");
+
+        $data = [
+            'freeDiskSpace' => $df,
+            'totalDiskSpace' => $dt
+        ];
+
+        return new JsonResponse($data);
+    }
+}
